453b91b60620ee8ba518bca281b1bcb908593acc
Raphael Yaadar <yaadar.5913211@gmail.com>
2022-10-05 01:25:49 +0000

    Added student service

---
+++ StudentManager/StudentManager/Domain/Services/StudentService.cs
@@ -0,0 +1,37 @@
+﻿using StudentManager.Models;
+
+namespace StudentManager.Domain.Services;
+
+public class StudentService : IStudentService
+{
+    private readonly IGenericRepository<Student> _repository;
+
+    public StudentService(IGenericRepository<Student> repository)
+    {
+        _repository = repository;
+    }
+
+    public IEnumerable<Student> GetAllStudents()
+    {
+        return _repository.GetAll();
+    }
+
+    public Student GetStudent(int id)
+    {
+        return _repository.GetById(id);
+    }
+
+    public void DeleteStudent(Student student)
+    {
+        _repository.Remove(student);
+    }
+}
+
+public interface IStudentService
+{
+    IEnumerable<Student> GetAllStudents();
+
+    Student GetStudent(int id);
+
+    void DeleteStudent(Student student);
+}
